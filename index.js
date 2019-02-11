a707572e6ab48387c0489b017936a3f51c6ada35
Dyanna Turner <dttheme@gmail.com>
2019-02-10 19:03:44 -0500

    Update site design

--- index.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 100) {
-    $('.navbar, .navbar-brand, li').addClass('shrink');
-  } else {
-    $('.navbar, .navbar-brand, li').removeClass('shrink');
-  }
-});
