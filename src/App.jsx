697ec200994d76bf50b1321ee5c8a67bcf97b492
Dyanna Turner <dttheme@gmail.com>
2024-01-10 23:19:18 -0500

    Cleanup, add react-nano

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default () => (
+  <div className="app-wrapper">
+  <Header>
+    <h1>Peach Butter Prints</h1>
+  </Header>
+  <Card>
+    </Card>
+    </div>
+);
