b22c944f1819dedda99b3f010be9c7917b8f5d12
dttheme <dttheme@gmail.com>
2024-01-17 17:24:06 -0500

    Copy updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Card from "./Card";
 import Github from "../svg/logo-github.svg";
 import Codepen from "../svg/logo-codepen.svg";
 import LinkedIn from "../svg/logo-linkedin.svg";
-import UnderContstruction from "../gifs/under-construction.gif";
 
 export default () => (
   <div className="app-wrapper">
@@ -12,32 +11,25 @@ export default () => (
       <h1>Dyanna Turner</h1>
     </Header>
     <Card>
-      <p>1.11.24: This website is under construction!</p>
-      <img
-        src={UnderContstruction}
-        alt="gif shows a flashing 'under construction' sign"
-      />
-    </Card>
-    <Card>
       <h2>About Me</h2>
+      <p>Hey! I'm Dyanna, an artist and software engineer living in Atlanta.</p>
       <p>
-        Greetings! I'm Dyanna, an artist and software engineer living in
-        Atlanta.
+        I've been working professionally as a fullstack software engineer since
+        2018. Most recently, I was Senior Software Engineer at Intuit Mailchimp.
+        I have past experience in React, Vue, Node, and Python.
       </p>
       <p>
-        Visit <a href="https://peachbutterprints.com">my other site</a> for more
-        information about my art.
+        At the end of 2023, I opened my art shop,{" "}
+        <a href="https://peachbutterprints.com">Peach Butter Prints</a>. You can
+        find links to my store and social media there.
+      </p>
+      <p>
+        I am looking for new engineering opportunities in 2024. I would love to
+        hear from you if you have a remote engineering position.
       </p>
     </Card>
     <Card>
       <div className="social">
-        <a
-          href="https://github.com/dttheme"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={Github} alt="Github" />
-        </a>
         <a
           href="https://www.linkedin.com/in/dyannaturner/"
           target="_blank"
@@ -45,6 +37,14 @@ export default () => (
         >
           <img src={LinkedIn} alt="LinkedIn" />
         </a>
+        <a
+          href="https://github.com/dttheme"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={Github} alt="Github" />
+        </a>
+
         <a
           href="https://codepen.io/dyannaturner/"
           target="_blank"
